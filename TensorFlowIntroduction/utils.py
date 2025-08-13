a76308c848bce94463cc925f6871921646594bb5
Abimael Franco <3006701680101@ingenieria.usac.edu.gt>
2025-08-12 22:49:05 -0600

    images_size function

--- TensorFlowIntroduction/utils.py
+++ TensorFlowIntroduction/utils.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import os
 
 def filter_images(DATASET_PATH: str, *namefolders: str, extension: str = ".JFIF") -> int:
@@ -20,7 +22,7 @@ def filter_images(DATASET_PATH: str, *namefolders: str, extension: str = ".JFIF"
     
     deleted_images = 0
 
-    for folder_name in (namefolders):
+    for folder_name in namefolders:
         folder_path = os.path.join(DATASET_PATH, folder_name)
         if not os.path.exists(folder_path):
             print(f"Carpeta no existe: {folder_path}")
@@ -44,3 +46,30 @@ def filter_images(DATASET_PATH: str, *namefolders: str, extension: str = ".JFIF"
             os.remove(img_path)
 
     return deleted_images
+
+def images_size(DATASET_PATH: str, *namefolders: str) -> None:
+    """
+    Displays up to 9 images from each specified folder and shows their dimensions.
+
+    Args:
+        DATASET_PATH (str): Base directory path where folders are located.
+        *namefolders (str): One or more folder names inside DATASET_PATH to process.
+
+    Returns:
+        None
+    """
+    for folder_name in namefolders:
+        folder_path = os.path.join(DATASET_PATH, folder_name)
+        images = os.listdir(folder_path)[:9]
+
+        plt.figure(figsize=(10, 10))
+        for i, image in enumerate(images):
+            img_path = os.path.join(folder_path, image)
+            img = mpimg.imread(img_path)
+            ax = plt.subplot(3, 3, i + 1)
+            plt.imshow(img)
+            plt.title(f"Size: {img.shape[0]} x {img.shape[1]} pixels")
+            plt.axis("off")
+
+        plt.suptitle(f"Folder: {folder_name}")
+        plt.show()
